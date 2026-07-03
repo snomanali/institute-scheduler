5120dc80ed3983fe2e98277c88b26b13b4ea5dc8
snomanali <149055527+snomanali@users.noreply.github.com>
2026-07-03 11:02:46 +0400

    Delete project directory

--- project/backend/src/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-// src/middleware/notFound.js
-const notFound = (req, res) => {
-  res.status(404).json({
-    success: false,
-    message: `Route ${req.method} ${req.url} not found.`,
-  });
-};
-
-module.exports = { notFound };
